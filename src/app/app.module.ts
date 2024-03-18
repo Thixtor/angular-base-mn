a2898baf83fcdcc941647b51cf4ab9bb7d68ba98
Sebastian Acosta <sebas.sas@gmail.com>
2024-03-18 16:49:29 -0500

    Practica Front-End

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsuarioComponent } from './usuario.component';
+import { FormsModule } from '@angular/forms';
+import { LibrosComponent } from './libros/libros.component'
+import { LibroComponent } from './libro/libro.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioComponent,
+    LibrosComponent,
+    LibroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
